cc8188949f4a7b5e228e9e6a7c87e2597d7e29f0
Sam Battat <hbattat@gmail.coms>
2018-12-22 18:38:17 -0800

    only replace host if the host and port are reachable for nodejs template

--- nodejs_tpl.sh
+++ nodejs_tpl.sh
@@ -1,7 +1,18 @@
+#!/bin/bash
 export VESTA=/usr/local/vesta/
 user=$1
 domain=$2
 nodejs_hostname=`/usr/local/vesta/bin/v-list-web-domain $user $domain | grep "PROXY EXT" | sed -r 's/.*nodejs_(.*)_([0-9]+).*/\1/g'`
 port_num=`/usr/local/vesta/bin/v-list-web-domain $user $domain | grep "PROXY EXT" | sed -r 's/.*nodejs_(.*)_([0-9]+).*/\2/g'`
 
-sed -i "s/NODEJS_HOSTNAME:65535/$nodejs_hostname:$port_num/g" /home/$user/conf/web/$domain.*
+re='^[0-9]+$'
+if ! [[ $port_number =~ $re ]]
+then
+  nc -vz -w1 $nodejs_hostname $port_num &>/dev/null
+  if [[ "$?" -eq "0" ]]
+  then
+    sed -i "s/NODEJS_HOSTNAME:65535/$nodejs_hostname:$port_num/g" /home/$user/conf/web/$domain.*
+  else
+    exit 64
+  fi
+fi
